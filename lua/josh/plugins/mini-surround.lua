da64e2004f08d6d4d5d59c52575dc048670d9bcc
Josh Tompkin <jtompkin-dev@pm.me>
2025-11-07 13:40:50 -0500

    add mini.surround prefix to mappings

--- lua/josh/plugins/mini-surround.lua
+++ lua/josh/plugins/mini-surround.lua
@@ -1,4 +1,14 @@
--- This plugin is lazy-loaded
+-- NOTE: This plugin is lazy-loaded
 Lib.pack_add_on_event("InsertEnter", { Lib.from_gh("nvim-mini/mini.surround") }, function()
-	require("mini.surround").setup({})
+	require("mini.surround").setup({
+		-- Add prefix to not mangle default 's' mapping
+		mappings = {
+			add = "<leader>sa",
+			delete = "<leader>sd",
+			find = "<leader>sf",
+			find_left = "<leader>sF",
+			highlight = "<leader>sh",
+			replace = "<leader>sr",
+		},
+	})
 end)
