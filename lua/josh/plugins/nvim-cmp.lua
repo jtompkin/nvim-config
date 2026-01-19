a1288809ce9844ff619183977876233dd63ac101
Josh Tompkin <jtompkin-dev@pm.me>
2026-01-19 17:59:17 -0500

    remove lazy-loading for nvim-cmp

--- lua/josh/plugins/nvim-cmp.lua
+++ lua/josh/plugins/nvim-cmp.lua
@@ -1,92 +1,86 @@
--- This plugin is lazy-loaded
--- Load mini.snippets before this plugin
-Lib.pack_add_on_event(
-	{ "InsertEnter", "CmdlineEnter" },
-	vim.tbl_map(Lib.from_gh, {
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets",
+vim.pack.add(vim.tbl_map(Lib.from_gh, {
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+}))
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+luasnip.filetype_extend("lua", { "luadoc" })
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_snipmate").lazy_load()
+require("luasnip.loaders.from_lua").lazy_load()
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-l>"] = cmp.mapping(function(fallback)
+			if luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function(fallback)
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<C-y>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if luasnip.expandable() then
+					luasnip.expand()
+				else
+					cmp.confirm({ select = true })
+				end
+			else
+				fallback()
+			end
+		end),
 	}),
-	function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		luasnip.filetype_extend("lua", { "luadoc" })
-		require("luasnip.loaders.from_vscode").lazy_load()
-		require("luasnip.loaders.from_snipmate").lazy_load()
-		require("luasnip.loaders.from_lua").lazy_load()
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-l>"] = cmp.mapping(function(fallback)
-					if luasnip.locally_jumpable(1) then
-						luasnip.jump(1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function(fallback)
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<C-y>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						if luasnip.expandable() then
-							luasnip.expand()
-						else
-							cmp.confirm({ select = true })
-						end
-					else
-						fallback()
-					end
-				end),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "lazydev", group_index = 0 },
-			}, {
-				{ name = "buffer" },
-			}),
-		})
-		local cmdline_mapping = cmp.mapping.preset.cmdline()
-		cmdline_mapping["<C-j>"] = cmdline_mapping["<Tab>"]
-		cmdline_mapping["<C-k>"] = cmdline_mapping["<S-Tab>"]
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmdline_mapping,
-			sources = {
-				{ name = "buffer" },
-			},
-		})
-		cmp.setup.cmdline(":", {
-			mapping = cmdline_mapping,
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{ name = "cmdline" },
-			}),
-			matching = { disallow_symbol_nonprefix_matching = false },
-		})
-	end
-)
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "lazydev", group_index = 0 },
+	}, {
+		{ name = "buffer" },
+	}),
+})
+local cmdline_mapping = cmp.mapping.preset.cmdline()
+cmdline_mapping["<C-j>"] = cmdline_mapping["<Tab>"]
+cmdline_mapping["<C-k>"] = cmdline_mapping["<S-Tab>"]
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmdline_mapping,
+	sources = {
+		{ name = "buffer" },
+	},
+})
+cmp.setup.cmdline(":", {
+	mapping = cmdline_mapping,
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false },
+})
