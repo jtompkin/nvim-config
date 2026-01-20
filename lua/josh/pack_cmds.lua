1f4a7f381c803d6d2958f3d912291c5e276f968f
Josh Tompkin <jtompkin-dev@pm.me>
2026-01-19 21:55:06 -0500

    add JSON lib

--- lua/josh/pack_cmds.lua
+++ lua/josh/pack_cmds.lua
@@ -14,51 +14,55 @@ local function packs_to_window(packs) ---@param packs string[]
 	local buf = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, packs)
 	vim.keymap.set("n", "q", vim.cmd.q, { buffer = buf })
-	local win = vim.api.nvim_open_win(
-		buf,
-		true,
-		{ relative = "win", row = 0, col = 3, width = 20, height = #packs, style = "minimal" }
-	)
+	local win = vim.api.nvim_open_win(buf, true, {
+		relative = "win",
+		row = 0,
+		col = 3,
+		height = #packs,
+		width = math.max(unpack(vim.tbl_map(function(pack) return #pack end, packs))) + 4 + math.floor(
+			math.log10(#packs)
+		),
+		style = "minimal",
+	})
 	vim.api.nvim_set_option_value("number", true, { scope = "local", win = win })
 end
 
-vim.api.nvim_create_user_command("PackList", function(args)
-	local arg = args.fargs[1]
-	if arg == "pretty" then
-		vim.pack.update(nil, { offline = true })
-		return
-	end
-	if arg == "inactive" then
-		packs_to_window(vim.tbl_map(
-			function(pack) ---@param pack Pack
-				return pack.spec.name
-			end,
-			vim.tbl_filter(function(pack) ---@param pack Pack
-				return not pack.active
-			end, vim.pack.get())
-		))
-		return
-	end
-	if vim.fn.executable("jq") == 1 then
-		local packs_string = vim.system({ "jq", "-r", ".plugins|keys[]", "nvim-pack-lock.json" }, { text = true })
-			:wait().stdout or ""
-		packs_to_window(vim.split(string.sub(packs_string, 1, #packs_string - 1), "\n"))
-	else
-		packs_to_window(vim.tbl_map(function(pack)
-			return pack.spec.name
-		end, vim.pack.get()))
-	end
-end, {
-	complete = function()
-		return { "pretty", "inactive", "all" }
+local list_dispatcher = { ---@type { [string]: fun(): nil }
+	pretty = function() vim.pack.update(nil, { offline = true }) end,
+	inactive = function()
+		packs_to_window(
+			vim.tbl_map(
+				function(pack) return pack.spec.name end,
+				vim.tbl_filter(function(pack) return not pack.active end, vim.pack.get())
+			)
+		)
 	end,
-	nargs = "?",
-	desc = "List installed packages",
-})
+	all = function()
+		if Lib.json then
+			io.input("nvim-pack-lock.json")
+			local pack_lock = Lib.json.decode(io.read("*a"))
+			packs_to_window(vim.tbl_keys(pack_lock.plugins or {}))
+		else
+			packs_to_window(vim.tbl_map(function(pack) return pack.spec.name end, vim.pack.get()))
+		end
+	end,
+}
 
-vim.api.nvim_create_user_command("PackDelete", function(args)
-	vim.pack.del(args.fargs)
-end, { nargs = "+", desc = "Delete installed packages from disk" })
+vim.api.nvim_create_user_command(
+	"PackList",
+	function(args) (list_dispatcher[args.fargs[1]] or list_dispatcher.all)() end,
+	{
+		complete = function() return vim.tbl_keys(list_dispatcher) end,
+		nargs = "?",
+		desc = "List installed packages",
+	}
+)
+
+vim.api.nvim_create_user_command(
+	"PackDelete",
+	function(args) vim.pack.del(args.fargs) end,
+	{ nargs = "+", desc = "Delete installed packages from disk" }
+)
 
 vim.api.nvim_create_user_command("PackUpdate", function(args)
 	local packs
