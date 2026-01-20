4a2a8c8dd684da52ebf4f81648c512d7e9324e53
Josh Tompkin <jtompkin-dev@pm.me>
2026-01-19 21:55:29 -0500

    add blink-cmp

---
+++ lua/josh/plugins/blink-cmp.lua
@@ -0,0 +1,37 @@
+vim.pack.add({
+	{ src = Lib.from_gh("Saghen/blink.cmp"), version = vim.version.range("~1") },
+	Lib.from_gh("rafamadriz/friendly-snippets"),
+})
+require("blink.cmp").setup({
+	keymap = {
+		preset = "default",
+		["<C-j>"] = { "select_next", "fallback" },
+		["<C-k>"] = { "select_prev", "fallback" },
+		["<C-l>"] = { "snippet_forward" },
+		["<C-h>"] = { "snippet_backward" },
+		["<C-e>"] = { "hide", "fallback" },
+		["<C-S-e>"] = { "cancel", "fallback" },
+		["<C-S-Space>"] = {
+			"show",
+			"show_documentation",
+			"hide_documentation",
+		},
+		["<C-g>"] = { "show_signature", "hide_signature", "fallback" },
+		["<C-u>"] = { "scroll_signature_up" },
+		["<C-d>"] = { "scroll_documentation_down" },
+		["<C-n>"] = false,
+		["<C-p>"] = false,
+	},
+	signature = { enabled = true },
+	completion = {
+		documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		ghost_text = { enabled = true },
+	},
+	sources = { providers = { snippets = { opts = { extended_filetypes = {
+		lua = { "luadoc" },
+	} } } } },
+	cmdline = {
+		keymap = { preset = "inherit" },
+		completion = { menu = { auto_show = true } },
+	},
+})
