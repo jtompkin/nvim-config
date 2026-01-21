333a4a391e8b96c3de8de596c1de65f4bbba7b7e
jtompkin <jtompkin-dev@pm.me>
2026-01-21 17:21:55 -0500

    updated completion for pack_cmds

--- lua/josh/pack_cmds.lua
+++ lua/josh/pack_cmds.lua
@@ -6,6 +6,8 @@
 ---@field spec vim.pack.SpecResolved
 ---@field tags? string[]
 
+---@alias error string? String describing error or `nil`
+
 local function packs_to_window(packs) ---@param packs string[]
 	if #packs == 0 then
 		vim.notify("No packages to show")
@@ -27,6 +29,21 @@ local function packs_to_window(packs) ---@param packs string[]
 	vim.api.nvim_set_option_value("number", true, { scope = "local", win = win })
 end
 
+---@return table val Decoded nvim-pack-lock.json file
+---@return error err Any error that occurred while parsing file
+local function get_packages_fast()
+	if not Lib.json then
+		return {}, "JSON library not loaded in Lib"
+	end
+	local f, err = io.open("nvim-pack-lock.json", "r")
+	if not f then
+		return {}, err
+	end
+	local pack_lock = Lib.json.decode(f:read("*a"))
+	f:close()
+	return pack_lock, nil
+end
+
 local list_dispatcher = { ---@type { [string]: fun(): nil }
 	pretty = function() vim.pack.update(nil, { offline = true }) end,
 	inactive = function()
@@ -41,17 +58,11 @@ local list_dispatcher = { ---@type { [string]: fun(): nil }
 		local function fallback()
 			packs_to_window(vim.tbl_map(function(pack) return pack.spec.name end, vim.pack.get()))
 		end
-		if Lib.json then
-			local f, err = io.open("nvim-pack-lock.json", "r")
-			if not f then
-				vim.notify("Could not open package lockfile:\n" .. err, vim.log.levels.ERROR)
-				fallback()
-				return
-			end
-			local pack_lock = Lib.json.decode(f:read("*a"))
-			f:close()
-			packs_to_window(vim.tbl_keys(pack_lock.plugins or {}))
+		local pack_lock, err = get_packages_fast()
+		if not err then
+			packs_to_window(vim.tbl_keys(pack_lock.plugins))
 		else
+			vim.notify("Could not open package lockfile:\n" .. err, vim.log.levels.ERROR)
 			fallback()
 		end
 	end,
@@ -67,16 +78,32 @@ vim.api.nvim_create_user_command(
 	}
 )
 
-vim.api.nvim_create_user_command(
-	"PackDelete",
-	function(args) vim.pack.del(args.fargs) end,
-	{ nargs = "+", desc = "Delete installed packages from disk" }
-)
+vim.api.nvim_create_user_command("PackDelete", function(args)
+	local opts = {}
+	if args.fargs[1] == "-f" then
+		opts.force = true
+		args.fargs = { unpack(args.fargs, 2) }
+	end
+	vim.pack.del(args.fargs, opts)
+end, {
+	nargs = "+",
+	complete = function() return vim.list_extend(vim.tbl_keys(get_packages_fast().plugins or {}), { "-f" }) end,
+	desc = "Delete installed packages from disk",
+})
 
 vim.api.nvim_create_user_command("PackUpdate", function(args)
+	local opts = {}
+	if args.fargs[1] == "-l" then
+		opts.target = "lockfile"
+		args.fargs = { unpack(args.fargs, 2) }
+	end
 	local packs
 	if #args.fargs > 0 then
 		packs = args.fargs
 	end
-	vim.pack.update(packs)
-end, { nargs = "*", desc = "Update installed packages" })
+	vim.pack.update(packs, opts)
+end, {
+	nargs = "*",
+	complete = function() return vim.list_extend(vim.tbl_keys(get_packages_fast().plugins or {}), { "-l" }) end,
+	desc = "Update installed packages",
+})
